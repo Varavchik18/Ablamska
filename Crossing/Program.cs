b978f41a706e6b253fa46487e66f6b5f41519d5b
Varavchik18 <48031083+Varavchik18@users.noreply.github.com>
2020-11-30 16:44:52 +0200

    final commit

--- Crossing/Program.cs
+++ Crossing/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Test
 {
+
     class Program
     {
         static void Main()
@@ -12,9 +13,8 @@ namespace Test
             string substitution = Console.ReadLine();
 
             var codeString = "using System;{1}{1} namespace Test{1}{2} {0}class Program{1} {0}{2}{1}{0}  static void Main() {1}{0}{0}{2}{1}{0}{0}{0} var s={6};{1}{0}{0}{0} Console.Write(s, {4}{5}t{4}, {4}{5}n{4}, {4}{2}{4}, {4}{3}{4}), {4}{5}{4}, {4}{5}{5}{4}, s; {1}{1} Console.Readline(); {1}{0}{0}{3} {1}{0}{3} {1}{3}";
-            Console.WriteLine(codeString, "\t", "\n", "{", "}", "\"", "\\", codeString);
 
-            Console.ReadLine();
+
             char[] QuineArray = codeString.ToCharArray();
             if (codeString.IndexOf(searchedValue, 0, codeString.Length) >= 0)
             {
@@ -23,7 +23,7 @@ namespace Test
 
                 Console.ReadLine();
             }
-            else 
+            else
             {
                 Console.WriteLine(ReverseString(codeString));
                 Console.ReadLine();
